7753c99a2818a431e15e6d6ef8052e05777b6fc1
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2024-01-09 17:38:48 -0500

    Add header component and add painting images to DOM

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,23 @@
 import "./App.css"
 import { useEffect, useState } from "react";
 import { getArtworks } from "../apiCall";
+import { Routes, Route } from 'react-router-dom'
+import MainPage from '../MainPage/MainPage'
 
 function App() {
   const [artworks, setArtworks] = useState([])
   useEffect(() => {
     getArtworks()
-    .then(artworkData => setArtworks(artworkData))
+    .then(artworkData => setArtworks(artworkData.records))
     .catch(err => console.error(err.message))
   }, [])
 
 
   return (
     <main className="article-app">
-      <h1>Article</h1>
+      <Routes>
+        <Route path="/" element={<MainPage artworks={artworks}/>} />
+      </Routes>
     </main>
   );
 }
