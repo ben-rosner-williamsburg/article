cb316678c18925bb39326fe79f1d1184f7bf60d8
Ben Rosner <ben.rosner.williamsburg@gmail.com>
2024-01-14 08:57:49 -0500

    Edit prop types for foavorites page and remove console logs

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,7 +4,6 @@ export const getArtworks = () => {
   return fetch(`http://api.harvardartmuseums.org/object?apikey=${REACT_APP_API_KEY}&hasimage=1&classification=Paintings&size=21&sort`)
   .then((response) => {
     if(response.ok){
-      console.log(response)
       return response.json()
     }
   })
@@ -15,7 +14,6 @@ export const getArtwork = (id) => {
   return fetch(`http://api.harvardartmuseums.org/object?apikey=${REACT_APP_API_KEY}&id=${id}`)
   .then((response) => {
     if(response.ok){
-      console.log(response)
       return response.json()
     }
   })
