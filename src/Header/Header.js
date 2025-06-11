763b7e3725885a9d542b6cc7b6c137457057574c
Ben Rosner <132025033+ben-rosner-williamsburg@users.noreply.github.com>
2025-06-11 09:09:35 -0400

    Fix Favorites Button Text Centering

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -40,10 +40,12 @@ const Header = () => {
               whileTap={{ scale: 0.95 }}
             >
               <Link className='favorite-link' to="/favorites">
-                <svg className="heart-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                  <path d="20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
-                </svg>
-                <span>Favorites</span>
+                <div className="favorite-link-content">
+                  <svg className="heart-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor">
+                    <path d="20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+                  </svg>
+                  <span className="favorite-text">Favorites</span>
+                </div>
               </Link>
             </motion.div>
           )}
